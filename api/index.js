fa79f6b3947c7e5cbea3b913835cc6e71af4ae26
Bernhard Steger <bernhard.steger@gmail.com>
2016-10-31 15:43:32 +0100

    add helmet protection

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var database = require('./config').database
+var helmet = require('helmet')
 
 // set port
 var port = process.env.PORT || 3000
@@ -14,7 +15,7 @@ mongoose.connect(database)
 // get routes from routes.js
 var router = require('./routes')
 
-
+app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
