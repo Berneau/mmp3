d69650452e630b7a4edf5b8ba09fbf231176d6ed
Bernhard Steger <bernhard.steger@gmail.com>
2017-03-24 14:13:29 +0100

    add class interfaces for product and vendor

---
+++ web/src/app/interfaces/vendor.ts
@@ -0,0 +1,15 @@
+export class Vendor {
+  _id?: string
+  name: string
+  ownerName: string
+  email: string
+  category: number
+  city: string
+  tel?: number
+  description?: string
+  street?: string
+  zip?: number
+  imageUrl?: string
+  lat?: number
+  long?: number
+}
