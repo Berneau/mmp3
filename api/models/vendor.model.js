295975173d333cefc890a3a6c08cf4dc33a78974
Bernhard Steger <bernhard.steger@gmail.com>
2017-04-25 14:25:03 +0200

    update mongoose models

--- api/models/vendor.model.js
+++ api/models/vendor.model.js
@@ -3,20 +3,21 @@ var Schema = mongoose.Schema
 var Product = require('./product.model')
 
 var VendorSchema = new Schema({
-  name: String,
-  ownerName: String,
-  email: String,
+  name: { type: String, required: true },
+  ownerName: { type: String, required: true },
+  email: { type: String, required: true },
+  category: { type: Number, required: true },
+  userUid: { type: String, required: true },
   tel: Number,
   description: String,
-  category: Number,
-  street: String,
-  zip: Number,
-  city: String,
   imageUrl: String,
-  // products: [Product],
-  lat: Number,
-  long: Number,
-  userId: String
+  address: {
+    city: String,
+    zip: Number,
+    street: String,
+    lat: Number,
+    long: Number
+  }
 })
 
 module.exports = mongoose.model('Vendor', VendorSchema)
