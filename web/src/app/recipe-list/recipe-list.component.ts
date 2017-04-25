bf114621274a84f1636c56e5b36fe106e6bd12e9
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-04-25 14:02:36 +0200

    add recipe list

--- web/src/app/recipe-list/recipe-list.component.ts
+++ web/src/app/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeService } from './../services/recipe.service'
+
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: RecipeService) { }
 
   ngOnInit() {
+    this.store.getRecipes()
   }
 
 }
--- web/src/app/services/recipe.service.ts
+++ web/src/app/services/recipe.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import { Recipe } from './../interfaces/recipe'
+import { ApiEndpoint } from './../app.config';
 
 @Injectable()
 export class RecipeService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  recipes: Recipe[]
+  recipe: Recipe
+  private apiEndpoint = ApiEndpoint
+
+  getRecipes() {
+    this.recipes = [
+      {
+        _id: '1',
+        name: 'Kartoffelknödel',
+        description: 'alles zammanschgan',
+        imageUrl: ''
+      },
+      {
+        _id: '2',
+        name: 'Kartoffelknödel',
+        description: 'alles zammanschgan',
+        imageUrl: ''
+      },
+      {
+        _id: '3',
+        name: 'Kartoffelknödel',
+        description: 'alles zammanschgan',
+        imageUrl: ''
+      },
+      {
+        _id: '4',
+        name: 'Kartoffelknödel',
+        description: 'alles zammanschgan',
+        imageUrl: ''
+      }
+    ]
+  }
+
+  private handleError(error: any) {
+    console.log(error.statusText, 2000)
+  }
 }
