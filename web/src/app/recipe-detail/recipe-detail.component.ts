61296f04ae88df482e89e2e3bf6b7b7dc1ffd98d
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-02 13:23:04 +0200

    add recipe-detail component

---
+++ web/src/app/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { RecipeService } from './../services/recipe.service'
+
+@Component({
+  selector: 'recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.sass']
+})
+export class RecipeDetailComponent implements OnInit {
+
+  constructor(private store: RecipeService, private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit() {
+    this.route.params.forEach((params) => {
+      let id = params['id']
+      this.store.getRecipe(id)
+      .then(recipe => {
+        if (!recipe) {
+          this.location.back()
+          Materialize.toast('Es wurde kein Rezept mit dieser ID gefunden.', 2000)
+          return
+        }
+      })
+    })
+  }
+
+}
