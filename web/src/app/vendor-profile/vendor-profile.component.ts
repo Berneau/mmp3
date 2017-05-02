3ba84956efaf3718a52c91b24ea6e02c451f2e26
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-02 17:29:03 +0200

    add content to vendor profile

--- web/src/app/vendor-profile/vendor-profile.component.ts
+++ web/src/app/vendor-profile/vendor-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Vendor } from './../interfaces/vendor'
 import { VendorService } from './../services/vendor.service'
 
 @Component({
@@ -13,6 +14,8 @@ export class VendorProfileComponent implements OnInit {
 
   constructor(private store: VendorService, private route: ActivatedRoute, private location: Location) { }
 
+  vendor: Vendor
+
   ngOnInit() {
     this.route.params.forEach((params) => {
       let id = params['id']
@@ -23,6 +26,8 @@ export class VendorProfileComponent implements OnInit {
           Materialize.toast('Es wurde kein Produzent mit dieser ID gefunden.', 2000)
           return
         }
+        this.vendor = vendor
+        console.log(this.vendor)
       })
     })
   }
