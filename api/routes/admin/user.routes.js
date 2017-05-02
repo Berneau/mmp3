c437d73b8fa6bea51c3109d80e8f7111fccf217f
Bernhard Steger <bernhard.steger@gmail.com>
2017-05-02 16:19:42 +0200

    update user routes - remove password from sent user object

--- api/routes/admin/user.routes.js
+++ api/routes/admin/user.routes.js
@@ -2,83 +2,83 @@ var User = require('../../models/user.model')
 
 module.exports = function(router) {
 
- router.route('/users/:id')
-   /**
-    * @api {get} /users/:id Get user
-    * @apiName GetUser
-    * @apiGroup Users
-    * @apiPermission admin
-    *
-    * @apiSuccess {Object} user The user for given id.
-   */
-   .get(function(req, res) {
-     User.findById(req.params.id, function(err, user) {
-       if (err) res.status(500).end(err)
+  router.route('/users')
+  /**
+  * @api {get} /users?filter=<filter> Get all users with optional search
+  * @apiName GetUsers
+  * @apiGroup Users
+  * @apiPermission admin
+  *
+  * @apiParam {String} [filter] The field name will be search by this.
+  *
+  * @apiSuccess {Array} user Array of users.
+  */
+  .get(function(req, res) {
 
-       if (user) res.status(200).json(user)
-       else res.status(404).json({ message: 'User not found'})
-     })
+  var filter = req.query.filter ? req.query.filter : ''
+
+  User
+  .find({ 'email': { '$regex': filter } }, function(err, users) {
+    if (err) res.status(500).end(err)
+    res.json(stripUserArray(users))
+  })
+  .sort({email: 1})
+  })
+
+  router.route('/users/:id')
+  /**
+  * @api {get} /users/:id Get user
+  * @apiName GetUser
+  * @apiGroup Users
+  * @apiPermission admin
+  *
+  * @apiSuccess {Object} user The user for given id.
+  */
+  .get(function(req, res) {
+   User.findById(req.params.id, function(err, user) {
+     if (err) res.status(500).end(err)
+
+     if (user) res.status(200).json(stripUserObject(user))
+     else res.status(404).json({ message: 'User not found'})
    })
+  })
 
-   /**
-    * @api {put} /users/:id Update user
-    * @apiName UpdateUser
-    * @apiGroup Users
-    * @apiPermission admin
-    *
-    * @apiParam {String} username The name of the user.
-    * @apiParam {String} email The email address of the user.
-    *
-    * @apiSuccess {Object} user The updated user.
-   */
-   .put(function(req, res) {
+  /**
+  * @api {delete} /users/:id Delete user
+  * @apiName DeleteUser
+  * @apiGroup Users
+  * @apiPermission admin
+  *
+  * @apiSuccess {String} message Success message.
+  */
+  .delete(function(req, res) {
+   User.findById(req.params.id, function(err, user) {
+     if (err) res.status(500).end(err)
 
-     User.findById(req.params.id, function(err, user) {
-       if (err) res.status(500).end(err)
+     if (user) {
+       User.remove({ _id: req.params.id }, function(err, user) {
+         if (err) res.status(500).end(err)
+         res.status(200).json({ message: 'Successfully deleted' })
+       })
+     } else res.status(404).json({ message: 'User not found'})
 
-       if (user) {
-         if (userIsValid(req.body)) {
-
-           user.username = req.body.name
-           user.email = req.body.email
-
-           user.save(function(err) {
-             if (err) res.status(500).end(err)
-             res.status(200).json(user)
-           })
-
-         } else res.status(412).json({ message: 'Missing fields' })
-
-       } else res.status(404).json({ message: 'User not found'})
-     })
    })
+  })
 
-   /**
-    * @api {delete} /users/:id Delete user
-    * @apiName DeleteUser
-    * @apiGroup Users
-    * @apiPermission admin
-    *
-    * @apiSuccess {String} message Success message.
-   */
-   .delete(function(req, res) {
-     User.findById(req.params.id, function(err, user) {
-       if (err) res.status(500).end(err)
+  function stripUserArray(arr) {
+   for (var i = 0; i < arr.length; i++) {
+     arr[i].password = undefined
+   }
+   return arr
+  }
 
-       if (user) {
-         User.remove({ _id: req.params.id }, function(err, user) {
-           if (err) res.status(500).end(err)
-           res.status(200).json({ message: 'Successfully deleted' })
-         })
-       } else res.status(404).json({ message: 'User not found'})
-
-     })
-   })
-
+  function stripUserObject(user) {
+    user.password = undefined
+    return user
+  }
 
   function userIsValid(user) {
-    if (!user.username ||
-        !user.password ||
+    if (!user.password ||
         !user.email ||
         !user.isAdmin) return false
     else return true
