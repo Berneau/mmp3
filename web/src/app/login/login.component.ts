59649faa208a30f2f8d73d4f079ecd9b832731f3
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-05 16:13:03 +0200

    add login and logout functions

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { LoginService } from './../services/login.service'
 
 @Component({
   selector: 'login',
@@ -9,18 +12,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private router: Router, private fb: FormBuilder, private store: LoginService) {
     this.createForm();
   }
 
   ngOnInit() {
+    this.store.logout()
   }
 
   createForm() {
     this.loginForm = this.fb.group({
-      name: '',
+      email: '',
       password: ''
     });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.store.login(this.loginForm.value.email, this.loginForm.value.password)
+      .then(result => {
+        if (result === true) {
+          // login successful
+          this.router.navigate(['/'])
+        } else {
+          // login failed
+          Materialize.toast('Login failed', 2000)
+        }
+      })
+    }
+    else Materialize.toast('Please fill in both fields', 2000)
   }
 
 }
--- web/src/app/services/login.service.ts
+++ web/src/app/services/login.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import { User } from './../interfaces/user'
+import { ApiEndpoint } from './../app.config'
 
 @Injectable()
 export class LoginService {
+  public token: string
+  private apiEndpoint = ApiEndpoint
+  private headers = new Headers({ 'Content-Type': 'application/json' })
+  private url = `${this.apiEndpoint}/user/login`
+  currentUser: User
 
-  constructor() { }
 
+  constructor(private http: Http) {
+    // set token if saved in local storage
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
+    this.token = this.currentUser && this.currentUser.token
+  }
+
+  login(email, password) {
+    return this.http
+      .post(this.apiEndpoint, JSON.stringify({ email, password }), { headers: this.headers })
+      .toPromise()
+      .then((response: Response) => {
+        // login successful if there's a jwt token in the response
+        let token = response.json() && response.json().token
+        let isAdmin = response.json() && response.json().user.admin
+
+        if (token && isAdmin) {
+          // set token property
+          this.token = token
+
+          //store username and jwt in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify({ email: email, token: token }))
+
+          console.log(response.json())
+
+          // return true to indicate successful login
+          return true
+        } else {
+          // return false to indicate failed login
+          return false
+        }
+      })
+      .catch(this.handleError)
+  }
+
+  logout(): void {
+    this.token = null
+    localStorage.removeItem('currentUser')
+  }
+
+  isLoggedIn(): boolean {
+    if (this.token) return true
+    else return false
+  }
+
+  handleError(err) {
+    Materialize.toast(err.message, 2000)
+  }
 }
