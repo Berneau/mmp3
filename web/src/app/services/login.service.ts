a4269b6f7f814802c6647872fe9fd8b48f7a1340
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-09 15:05:29 +0200

    edit login function

--- web/src/app/services/login.service.ts
+++ web/src/app/services/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Headers, Http, Response } from '@angular/http';
 
 import { User } from './../interfaces/user'
@@ -7,17 +8,17 @@ import { ApiEndpoint } from './../app.config'
 @Injectable()
 export class LoginService {
   public token: string
+  public currentUserIsAdmin: boolean
   private apiEndpoint = ApiEndpoint
   private headers = new Headers({ 'Content-Type': 'application/json' })
   private url = `${this.apiEndpoint}/auth`
   currentUser: User
-  currentUserIsAdmin: boolean
 
-
-  constructor(private http: Http) {
+  constructor(private http: Http, private route: ActivatedRoute) {
     // set token if saved in local storage
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
     this.token = this.currentUser && this.currentUser.token
+    this.currentUserIsAdmin = this.currentUser && this.currentUser.isAdmin
   }
 
   login(email, password) {
@@ -27,15 +28,12 @@ export class LoginService {
       .then((response: Response) => {
         // login successful if there's a jwt token in the response
         let token = response.json() && response.json().token
-        let isAdmin = response.json() && response.json().user.isAdmin
-
-        this.currentUserIsAdmin = isAdmin
 
         if (token) {
           // set token property
           this.token = token
           //store username and jwt in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify({ email: email, token: token }))
+          localStorage.setItem('currentUser', JSON.stringify({ email: email,  _id: response.json().user._id, isAdmin: response.json().user.isAdmin, token: token}))
 
           // return true to indicate successful login
           return true
@@ -59,9 +57,22 @@ export class LoginService {
   }
 
   isAdmin(): boolean {
+    // console.log("isAdmin")
     if (this.currentUserIsAdmin) return true
     else return false
   }
+
+  // getUser(id): Promise<any> {
+  //   let url = `${this.apiEndpoint}/users/${id}`
+  //
+  //   return this.http
+  //     .get(url)
+  //     .toPromise()
+  //     .then((res) => {
+  //       return res.json() as User
+  //     })
+  //     .catch(this.handleError)
+  // }
 
   handleError(err) {
     Materialize.toast(err.message, 2000)
