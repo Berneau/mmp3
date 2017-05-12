60e06d8b44352aaf2e21576fcfb2b7fea5dee9cd
Bernhard Steger <bernhard.steger@gmail.com>
2017-05-12 10:21:04 +0200

    add email check

--- api/helpers/helpers.js
+++ api/helpers/helpers.js
@@ -44,5 +44,9 @@ module.exports = {
      arr[i].salt = undefined
    }
    return arr
-  }
+ },
+  emailIsValid: function (email) {
+    var re = /\S+@\S+\.\S+/
+    return re.test(email)
+}
 }
--- api/routes/unauthenticated/user.routes.js
+++ api/routes/unauthenticated/user.routes.js
@@ -3,6 +3,7 @@ var password = require('s-salt-pepper')
 var User = require('../../models/user.model')
 var userIsValid = require('../../helpers/helpers').userIsValid
 var stripUserObject = require('../../helpers/helpers').stripUserObject
+var emailIsValid = require('../../helpers/helpers').emailIsValid
 
 module.exports = function(router) {
 
@@ -23,48 +24,59 @@ module.exports = function(router) {
 
    if (userIsValid(req.body)) {
 
-     password.hash(req.body.password, function(err, salt, hash) {
+     if (emailIsValid(req.body.email)) {
 
-       // error during hashing
-       if (err) res.status(500).json({
-         ok: false,
-         err: err.message
+       password.hash(req.body.password, function(err, salt, hash) {
+
+         // error during hashing
+         if (err) res.status(500).json({
+           ok: false,
+           err: err.message
+         })
+
+         else {
+           var user = new User({
+             email: req.body.email,
+             password: hash,
+             salt: salt,
+             isAdmin: req.body.isAdmin
+           })
+
+           user.save(function(err) {
+             if (err) {
+
+               // Err Code 11000 = duplicate Key in MongoDB, email already exists
+               if (err.code == 11000) res.status(200).json({
+                 ok: false,
+                 message: 'Email already taken.'
+               })
+
+               // internal server error
+               else res.status(500).json({
+                 ok: false,
+                 err: err.message
+               })
+             }
+
+             // return created user object
+             else res.status(200).json({
+               ok: true,
+               user: stripUserObject(user)
+             })
+           })
+         }
+
        })
 
-       else {
-         var user = new User({
-           email: req.body.email,
-           password: hash,
-           salt: salt,
-           isAdmin: req.body.isAdmin
-         })
+     }
 
-         user.save(function(err) {
-           if (err) {
-
-             // Err Code 11000 = duplicate Key in MongoDB, email already exists
-             if (err.code == 11000) res.status(200).json({
-               ok: false,
-               message: 'Email already taken.'
-             })
-
-             // internal server error
-             else res.status(500).json({
-               ok: false,
-               err: err.message
-             })
-           }
-
-           // return created user object
-           else res.status(200).json({
-             ok: true,
-             user: stripUserObject(user)
-           })
-         })
-       }
-
+     // no valid email address
+     else res.status(412).json({
+       ok: false,
+       message: 'Not a valid email address'
      })
 
+     // no valid user object
    } else res.status(412).json({
      ok: false,
      message: 'Not a valid user object'
