95a3727a0667c25b096bc9ff7c633000e2abde4d
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-20 15:44:25 +0200

    add forms for category type and user

---
+++ web/src/app/type-form/type-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'type-form',
+  templateUrl: './type-form.component.html',
+  styleUrls: ['./type-form.component.less']
+})
+export class TypeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
