5e818850061ef380dce72cf91bc4ca8e32411094
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-20 18:08:08 +0200

    add add- & get-function for types

--- web/src/app/services/type.service.ts
+++ web/src/app/services/type.service.ts
@@ -7,9 +7,21 @@ import { ApiEndpoint } from './../app.config'
 @Injectable()
 export class TypeService {
 
+  types: Type[]
   private apiEndpoint = ApiEndpoint
 
   constructor(private http: Http) { }
+
+  getTypes(): Promise<any> {
+    let url = `${this.apiEndpoint}/types`
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((res) => {
+        this.types = res.json().types
+      })
+      .catch(this.handleError)
+  }
 
   getType(id): Promise<any> {
     let url = `${this.apiEndpoint}/types/${id}`
@@ -18,6 +30,27 @@ export class TypeService {
       .get(url)
       .toPromise()
       .then((res) => {
+        return res.json().type as Type
+      })
+      .catch(this.handleError)
+  }
+
+  addType(form) {
+    let url = `${this.apiEndpoint}/types`
+    let token = JSON.parse(localStorage.getItem('currentUser')).token
+    let authHeaders = new Headers({
+      'Content-Type': 'application/json', 'x-access-token': token
+    })
+
+    let t = {
+      name: form.name
+    }
+
+    return this.http
+      .post(url, JSON.stringify(t), { headers: authHeaders })
+      .toPromise()
+      .then((res: Response) => {
+        this.getTypes()
         return res.json().type as Type
       })
       .catch(this.handleError)
