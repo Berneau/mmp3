8cc47a5731d3c7925d3d17ea23c9168383ff9897
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-20 18:05:52 +0200

    edit event-form

--- web/src/app/event-form/event-form.component.ts
+++ web/src/app/event-form/event-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Event } from './../interfaces/event'
 
 @Component({
   selector: 'event-form',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventFormComponent implements OnInit {
 
-  constructor() { }
+  eventForm: FormGroup
+  @Input() event: Event
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm()
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+  }
+
+  createForm() {
+    if (this.event) {
+      this.eventForm = this.fb.group({
+        name: this.event.name,
+        date: this.event.date,
+        description: this.event.description,
+        location: this.fb.group({
+          name: this.event.location.name,
+          lat: this.event.location.lat,
+          long: this.event.location.long
+        })
+      });
+    }
+    else {
+      this.eventForm = this.fb.group({
+        name: '',
+        date: '',
+        description: '',
+        location: this.fb.group({
+          name: '',
+          lat: '',
+          long: ''
+        })
+      });
+    }
   }
 
 }
