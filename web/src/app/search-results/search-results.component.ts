b1b39ad46c31732681d21f002e2a89172561f953
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-20 14:45:56 +0200

    edit search result page

--- web/src/app/search-results/search-results.component.ts
+++ web/src/app/search-results/search-results.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SearchService } from './../services/search.service'
+
+import { Vendor } from './../interfaces/vendor'
+import { Category } from './../interfaces/category'
+import { Event } from './../interfaces/event'
 
 @Component({
   selector: 'search-results',
@@ -7,9 +14,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  vendorResults: Vendor[]
+  categoryResults: Category[]
+  eventResults: Event[]
+
+  constructor(private SearchStore: SearchService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params) => {
+      let word = params['search']
+      this.SearchStore.getVendorResults(word)
+        .then(vendorResults => {
+          if (!vendorResults) {
+            this.vendorResults = []
+            return
+          }
+          console.log(vendorResults)
+          this.vendorResults = vendorResults
+          console.log(this.vendorResults)
+        })
+      this.SearchStore.getCategoryResults(word)
+        .then(categoryResults => {
+          if (!categoryResults) {
+            this.categoryResults = []
+            return
+          }
+          console.log(categoryResults)
+          this.categoryResults = categoryResults
+          console.log(this.categoryResults)
+        })
+      this.SearchStore.getEventResults(word)
+        .then(eventResults => {
+          if (!eventResults) {
+            this.eventResults = []
+            return
+          }
+          console.log(eventResults)
+          this.eventResults = eventResults
+          console.log(this.eventResults)
+        })
+    })
   }
 
 }
