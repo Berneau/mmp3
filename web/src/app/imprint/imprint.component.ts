ff395e5f578b1292ef1da38ca4672ac6ba5bbe4d
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-22 19:52:27 +0200

    add imprint page

---
+++ web/src/app/imprint/imprint.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'imprint',
+  templateUrl: './imprint.component.html',
+  styleUrls: ['./imprint.component.less']
+})
+export class ImprintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
