456c406fa8c9b874bd528e1342e40fca246c0459
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-22 14:18:27 +0200

    edit admin dashboard

--- web/src/app/admin-dashboard/admin-dashboard.component.ts
+++ web/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MzModalService } from 'ng2-materialize';
 
-import { UserService } from './../services/user.service'
-import { VendorService } from './../services/vendor.service'
-import { TypeService } from './../services/type.service'
-import { EventService } from './../services/event.service'
-
 import { UserFormComponent } from './../user-form/user-form.component'
 import { CategoryFormComponent } from './../category-form/category-form.component'
 import { TypeFormComponent } from './../type-form/type-form.component'
@@ -19,7 +14,7 @@ import { EventFormComponent } from './../event-form/event-form.component'
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor(private UserStore: UserService, private VendorStore: VendorService, private TypeStore: TypeService, private EventStore: EventService, private modalService: MzModalService) { }
+  constructor(private modalService: MzModalService) { }
 
   ngOnInit() {
   }
@@ -34,7 +29,7 @@ export class AdminDashboardComponent implements OnInit {
   }
 
   openNewTypeModal() {
-    this.modalService.open(PostitFormComponent);
+    this.modalService.open(TypeFormComponent);
   }
 
   openNewPostitModal() {
@@ -46,35 +41,4 @@ export class AdminDashboardComponent implements OnInit {
     this.modalService.open(PostitFormComponent);
     // TODO: change
   }
-
-  newUser(form) {
-    console.log("add User", form)
-  }
-
-  newVendor(form) {
-    console.log("add Vendor", form)
-  }
-
-  newType(form) {
-    this.TypeStore.addType(form.typeForm.value)
-      .then(type => {
-        if (!type) {
-          Materialize.toast('Hinzufügen fehlgeschlagen.', 2000)
-          return
-        }
-        Materialize.toast('Typ gespeichert.', 2000)
-      })
-  }
-
-  newEvent(form) {
-    this.EventStore.addEvent(form.eventForm.value)
-      .then(event => {
-        if (!event) {
-          Materialize.toast('Hinzufügen fehlgeschlagen.', 2000)
-          return
-        }
-        Materialize.toast('Event gespeichert.', 2000)
-      })
-  }
-
 }
