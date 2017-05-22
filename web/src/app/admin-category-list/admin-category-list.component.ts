c94d9966c237170d0a2eb9294dcd02bd9df48f70
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-22 13:04:19 +0200

    add admin category list component

---
+++ web/src/app/admin-category-list/admin-category-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-category-list',
+  templateUrl: './admin-category-list.component.html',
+  styleUrls: ['./admin-category-list.component.less']
+})
+export class AdminCategoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
