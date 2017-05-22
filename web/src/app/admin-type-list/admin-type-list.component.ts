6f309af9fc1b707e591328e7c8378606df5f3845
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-22 13:04:46 +0200

    add admin type list component

---
+++ web/src/app/admin-type-list/admin-type-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-type-list',
+  templateUrl: './admin-type-list.component.html',
+  styleUrls: ['./admin-type-list.component.less']
+})
+export class AdminTypeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
