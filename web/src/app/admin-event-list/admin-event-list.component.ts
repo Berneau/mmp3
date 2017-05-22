3adf56284ec374d2615ad4f070383dd79e782f19
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-22 13:04:33 +0200

    add admin event list component

---
+++ web/src/app/admin-event-list/admin-event-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-event-list',
+  templateUrl: './admin-event-list.component.html',
+  styleUrls: ['./admin-event-list.component.less']
+})
+export class AdminEventListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
