7dfc584d59dd08eb496f11afd88aa75ee7bf035f
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-05-26 22:07:17 +0200

    add sitemap

--- web/src/app/sitemap/sitemap.component.ts
+++ web/src/app/sitemap/sitemap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sitemap',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SitemapComponent implements OnInit {
 
-  constructor() { }
+  sites: Object[]
+
+  constructor(private router: Router) {
+    this.sites = []
+  }
 
   ngOnInit() {
+    this.getSites()
+  }
+
+  getSites() {
+    for (let r of this.router.config) {
+      if (r.component && !r.canActivate && r.path !== '404' && r.path !== 'suche') {
+        if (r.path === '') {
+          this.sites.push({ name: 'Home', path: '' })
+        }
+        else {
+          let name = r.path.slice(0, 1).toUpperCase() + r.path.slice(1)
+          this.sites.push({ name: name, path: `/${r.path}` })
+        }
+      }
+    }
   }
 
 }
