b9b11e21c73209906dfa84cb24352fe18bf53be6
Magdalena Wimmer <magdalena.wimmer@namics.com>
2017-06-08 00:25:18 +0200

    edit style

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { config } from './../config'
-// Import the Amazon S3 service client
-import * as AWS from 'aws-sdk'
 
 @Component({
   selector: 'app-root',
@@ -14,32 +11,5 @@ export class AppComponent {
   }
 
   ngOnInit(): void {
-    this.initS3();
-  }
-
-  initS3() {
-    AWS.config.accessKeyId = config.s3Credentials.accessKeyId
-    AWS.config.secretAccessKey = config.s3Credentials.secretAccessKey
-    AWS.config.region = config.s3Credentials.bucketRegion
-
-    let params = {
-      Bucket: 'lungau',
-    };
-
-    let sts = new AWS.STS();
-    sts.getSessionToken(function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else console.log(data);           // successful response
-    });
-
-    // let s3 = new AWS.S3({
-    //   apiVersion: '2006-03-01',
-    //   accessKeyId: config.s3Credentials.accessKeyId,
-    //   secretAccessKey: config.s3Credentials.secretAccessKey,
-    //   region: config.s3Credentials.bucketRegion,
-    //   sessionToken: ''
-    // });
-    //
-
   }
 }
